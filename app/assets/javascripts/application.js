9cb36071b453b6db49b56fd049d4d59e82ae0aa2
RJ <rjrobinson@gmail.com>
2015-07-08 23:02:18 -0400

    corrected js requirements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,5 @@
 //= require bootstrap-sprockets
 //= require underscore
 //= require gmaps/google
+//= require gmaps
+//= require gmapslib
--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,21 @@
 # require "resque_web"
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+	devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
 
 
-  root 'incidents#welcome'
+	root 'incidents#welcome'
 
-  resources :incidents do
-    resources :updates
-    resources :comments
-  end
+	resources :incidents do
+		resources :updates
+		resources :comments
+	end
 
-  resources :users
+	resources :users
 
-
-
-
-
-  # Background wrokers!  ** BETA VERSION **
-  # After devise is installed, wrap this in an authenticate
-  #
-  # ResqueWeb::Engine.eager_load!
-  # mount ResqueWeb::Engine => "/resque_web"
-
-
-
-
-
-
-
-
-
-
+	# Background wrokers!  ** BETA VERSION **
+	# After devise is installed, wrap this in an authenticate
+	#
+	# ResqueWeb::Engine.eager_load!
+	# mount ResqueWeb::Engine => "/resque_web"
 end
